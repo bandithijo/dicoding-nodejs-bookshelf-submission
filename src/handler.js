721976b3a8632b33015133171783b68010630d2f
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2024-05-12 18:58:57 +0800

    add: filter by reading, finished, name for getAllBooksHandler

--- src/handler.js
+++ src/handler.js
@@ -45,16 +45,35 @@ const addBookHandler = (request, h) => {
   };
 };
 
-const getAllBooksHandler = () => ({
-  status: 'success',
-  data: {
-    books: books.map((book) => ({
-      id: book.id,
-      name: book.name,
-      publisher: book.publisher
-    })),
-  },
-});
+const getAllBooksHandler = (request, h) => {
+  const { reading, finished, name } = request.query;
+
+  let filteredBooks = books;
+  if (reading === '1') {
+    filteredBooks = books.filter((book) => book.reading === true);
+  } else if (reading === '0') {
+    filteredBooks = books.filter((book) => book.reading === false);
+  } else if (finished === '1') {
+    filteredBooks = books.filter((book) => book.finished === true);
+  } else if (finished === '0') {
+    filteredBooks = books.filter((book) => book.finished === false);
+  } else if (name) {
+    filteredBooks = books.filter((book) => book.name.toLowerCase().includes(name.toLowerCase()));
+  };
+
+  const response = h.response({
+    status: 'success',
+    data: {
+      books: filteredBooks.map((book) => ({
+        id: book.id,
+        name: book.name,
+        publisher: book.publisher,
+      })),
+    },
+  });
+  response.code(200);
+  return response;
+};
 
 const getBookByIdHandler = (request, h) => {
   const { bookId } = request.params;
